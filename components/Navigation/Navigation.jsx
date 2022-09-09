927c5cc933249fdb4f0b01bb70b879b3e58b48ca
Hubert Szczepaniak <hubert.szczepaniak93@gmail.com>
2022-09-09 15:03:17 +0200

    burger menu

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 import cabanaLabsLogo from '../../public/images/cabana_labs_logo.png';
+import { BurgerMenu } from './BurgerMenu';
+import { navData } from './navigationData';
 
 export const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const onLogoClickHandler = () => {
     if (typeof window !== 'undefined') {
       window.scrollTo(0, 0);
@@ -18,16 +24,42 @@ export const Navigation = () => {
         >
           <Image alt='Cabana Labs Logo' src={cabanaLabsLogo} quality={100} />
         </div>
+        <motion.div
+          className='sm:hidden'
+          initial={false}
+          animate={isOpen ? 'open' : 'closed'}
+          custom='100%'
+        >
+          <BurgerMenu toggle={() => setIsOpen(prevState => !prevState)} />
+          <motion.div
+            className='bg-white opacity-50 p-10 fixed top-[72px] right-0'
+            variants={{
+              closed: {
+                opacity: 0,
+                x: 100,
+              },
+              open: {
+                opacity: 1,
+                x: 0,
+              },
+            }}
+            transition={{ duration: 0.1 }}
+          >
+            <ul className='flex flex-col gap-4 '>
+              {navData.map(item => (
+                <li key={item.label} className='nav-item'>
+                  <a href={item.to}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        </motion.div>
         <ul className='hidden sm:flex gap-12 '>
-          <li className='nav-item'>Home</li>
-          <li className='nav-item'>About</li>
-          <li className='nav-item'>
-            <a href='#services'>Services</a>
-          </li>
-          <li className='nav-item'>
-            <a href='#contact'>Contact</a>
-          </li>
-          <li className='nav-item'>Documentation</li>
+          {navData.map(item => (
+            <li key={item.label} className='nav-item'>
+              <a href={item.to}>{item.label}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
---
+++ components/Navigation/navigationData.js
@@ -0,0 +1,22 @@
+export const navData = [
+  {
+    label: 'Home',
+    to: '#',
+  },
+  {
+    label: 'About',
+    to: '#',
+  },
+  {
+    label: 'Services',
+    to: '#',
+  },
+  {
+    label: 'Contact',
+    to: '#',
+  },
+  {
+    label: 'Documentation',
+    to: '#',
+  },
+];
