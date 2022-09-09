c7d5beddc79d742a05f902cb57359f511f03116d
Hubert Szczepaniak <hubert.szczepaniak93@gmail.com>
2022-09-09 15:33:07 +0200

    z-index added

--- sections/HeroSection/HeroSection.jsx
+++ sections/HeroSection/HeroSection.jsx
@@ -6,7 +6,7 @@ export const HeroSection = () => {
   return (
     <section className='relative overflow-hidden'>
       <div className='min-h-[500px] md:min-h-[717px] flex justify-center items-center section-content'>
-        <div className='bg-white opacity-95 p-10 md:p-20 rounded-2xl drop-shadow-regular'>
+        <div className='bg-white opacity-95 p-10 md:p-20 rounded-2xl drop-shadow-regular z-10'>
           <h1 className='text-2xl md:text-4xl font-medium'>
             Security is hard.
             <br />
