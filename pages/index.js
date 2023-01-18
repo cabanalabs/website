a9e11834d1271744b63d67a7c1d4e2c385cc169a
Frank Fox <ffox77@gmail.com>
2023-01-18 10:24:17 -0800

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,19 @@ import {
   FAQSection,
 } from '../sections';
 
+import heroBg from '../public/images/hero_bg_47331.jpg';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Cabana Labs</title>
-        <meta property='og:title' content='Cabana Labs' key='title' />
-        <meta name='description' content='Cabana Labs' key='description' />
+        <meta property='og:title' content='Cabana Labs'  />
+        <meta property='og:description' content="Boost utility in your project with Cabana Labs' digital identity solutions for Web3. Offer your members decentralized profiles, membership credentials and engagement badges" />
+        <meta property='og:image' content={heroBg}  />
+        <meta property="og:image:alt" content="An image featuring two mobile phones at different angles showing different screens of the Cabana identity app." />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="1000" />
       </Head>
       <>
         {/*<div>*/}
