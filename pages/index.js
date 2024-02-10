d14c630cd5e870b147289a7cf9c0678b0103b1e8
Frank Fox <ffox77@gmail.com>
2024-02-10 11:28:57 -0800

    add preview cover image

--- pages/index.js
+++ pages/index.js
@@ -13,20 +13,20 @@ export default function Home() {
         <title>Cabana Labs</title>
         <meta
           name="description"
-          content="Boost utility in your project with Cabana Labs digital identity solutions for Web3. Offer your members decentralized profiles, membership credentials and engagement badges"
+          content="Build stronger communities with Cabana Labs. Go beyond anonymous avatars and faceless interactions with a robust framework. Offer members decentralized profiles, membership credentials and engagement badges."
         />
         <meta property="og:title" content="Cabana Labs" />
         <meta
           property="og:description"
-          content="Boost utility in your project with Cabana Labs digital identity solutions for Web3. Offer your members decentralized profiles, membership credentials and engagement badges"
+          content="Build stronger communities with Cabana Labs. Go beyond anonymous avatars and faceless interactions with a robust framework. Offer members decentralized profiles, membership credentials and engagement badges."
         />
-        <meta property="og:image" content="https://cabanalabs.com/images/hero_bg_47331_sm.png" />
+        <meta property="og:image" content="https://cabanalabs.com/images/hero_bg_v3.jpg" />
         <meta
           property="og:image:alt"
-          content="An image featuring two mobile phones at different angles showing different screens of the Cabana identity app."
+          content="An image featuring three digital identities each has a person avatar image and profile score."
         />
-        <meta property="og:image:width" content="360" />
-        <meta property="og:image:height" content="300" />
+        <meta property="og:image:width" content="1686" />
+        <meta property="og:image:height" content="960" />
       </Head>
       <>
         <HeroSection />
