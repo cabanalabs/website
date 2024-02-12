c663d2b6b427e43ce5b0bf10e6fe2acce3775b7b
Frank Fox <ffox77@gmail.com>
2024-02-12 09:54:21 -0800

    update link preview

--- pages/index.js
+++ pages/index.js
@@ -11,27 +11,21 @@ export default function Home() {
     <>
       <Head>
         <title>Cabana Labs</title>
-        <meta
-          name="description"
-          content="Build stronger communities with Cabana Labs. Go beyond anonymous avatars and faceless interactions with a robust framework. Offer members decentralized profiles, membership credentials and engagement badges."
-        />
-        <meta property="og:title" content="Cabana Labs" />
-        <meta
-          property="og:description"
-          content="Build stronger communities with Cabana Labs. Go beyond anonymous avatars and faceless interactions with a robust framework. Offer members decentralized profiles, membership credentials and engagement badges."
-        />
-        <meta property="og:image" content="https://cabanalabs.com/images/hero_bg_v3.jpg" />
-        <meta
-          property="og:image:alt"
-          content="An image featuring three digital identities each has a person avatar image and profile score."
-        />
-        <meta property="og:image:width" content="1686" />
-        <meta property="og:image:height" content="960" />
+        <meta name="description" content="Build stronger communities with Cabana Labs. Go beyond anonymous avatars and faceless interactions with a robust framework. Offer members decentralized profiles, membership credentials and engagement badges."/>
+        <meta property="og:title" content="Cabana Labs"/>
+        <meta property="og:description" content="Build stronger communities with Cabana Labs. Go beyond anonymous avatars and faceless interactions with a robust framework."/>
+        <meta property="og:image" content="https://cabanalabs.com/images/hero_bg_v3b.jpg"/>
+        <meta property="og:image:alt" content="An image featuring three digital identities each has a person avatar image and profile score."/>
+        <meta property="og:image:width" content="1200"/>
+        <meta property="og:image:height" content="630"/>
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:site" content="@cabana_labs"/>
+        <meta property="twitter:title" content="Cabana Labs"/>
+        <meta property="twitter:description" content="Build stronger communities with Cabana Labs. Go beyond anonymous avatars and faceless interactions with a robust framework."/>
+        <meta property="twitter:image" content="https://cabanalabs.com/images/hero_bg_v3_twitter.jpg"/>
       </Head>
       <>
-        <HeroSection />
-        {/*<HowItWorks />*/}
-        {/*<BuildTogether />*/}
+        <HeroSection/> {/*<HowItWorks />*/} {/*<BuildTogether />*/}
         <TrustScores />
         <IdentityBuilding />
         <SDK />
