e1fa999d6d40569833ed5a911879e11fb7bd7d2a
Frank Fox <ffox77@gmail.com>
2024-03-29 08:00:17 -0700

    add bg scrolling

--- components/PageLayout/PageLayout.jsx
+++ components/PageLayout/PageLayout.jsx
@@ -2,10 +2,10 @@ import { Navigation, Footer } from '../../components';
 
 export const PageLayout = ({ children }) => {
   return (
-    <>
+    <div className="parallax">
       <Navigation />
       <main className='relative'>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
