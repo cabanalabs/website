0d3dfa71430529ee15194f66fd02a684717321cb
Frank Fox <ffox77@gmail.com>
2024-03-29 08:40:02 -0700

    fix team card bg

--- components/TeamCard/TeamCard.jsx
+++ components/TeamCard/TeamCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const TeamCard = ({ name, title, score, avatar }) => {
   return (
-    <div className="flex justify-between min-w-full sm:min-w-[340px] lg:min-w-[440px] p-6 bg-white/[.06] rounded-2xl shadow-regular snap-center">
+    <div className="flex justify-between min-w-full sm:min-w-[340px] lg:min-w-[440px] p-6 bg-black  rounded-2xl shadow-regular snap-center ">
       <div className="flex flex-col gap-6">
         <div>
           <Image className="rounded-full overflow-hidden" src={avatar} width={92} height={92} alt={name} />
